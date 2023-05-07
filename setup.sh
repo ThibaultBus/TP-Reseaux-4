292b3b665923a94a5a02ac58d9fcd660c4d751ba
Corentin Gouil <gouilcorentin@gmail.com>
2023-05-07 18:48:50 +0200

    fix script

--- setup.sh
+++ setup.sh
@@ -3,5 +3,3 @@
 sudo modprobe -v dummy numdummies=2
 sudo ip a add 166.166.1.1/24 dev dummy0
 sudo ip link set dummy0 up
-
-sudo /usr/bin/vmhgfs-fuse .host:/ /home/corentingouil/shares -o subtype=vmhgfs-fuse,allow_other
